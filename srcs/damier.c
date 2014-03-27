8e5c724dc9603af470dc17d96e43faafc7b446f0
Quentin De Vial <qde-vial@student.42.fr>
2014-03-27 15:10:38 +0100

    norme

--- srcs/damier.c
+++ srcs/damier.c
@@ -6,7 +6,7 @@
 /*   By: qde-vial <qde-vial@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/03/25 13:29:57 by qde-vial          #+#    #+#             */
-/*   Updated: 2014/03/25 13:41:14 by qde-vial         ###   ########.fr       */
+/*   Updated: 2014/03/27 15:10:00 by qde-vial         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,8 +27,8 @@ void		calc_dam(t_obj *obj, double *k)
 	else
 	{
 		if (((int)fabs(obj->pos.x) % 100 <= 50 && (int)fabs(obj->pos.y)
-		 % 100 <= 50) || ((int)fabs(obj->pos.x) % 100 > 50 &&
-		 (int)fabs(obj->pos.y) % 100 > 50))
+			% 100 <= 50) || ((int)fabs(obj->pos.x) % 100 > 50 &&
+			(int)fabs(obj->pos.y) % 100 > 50))
 			;
 		else
 			k[0] = -1;
